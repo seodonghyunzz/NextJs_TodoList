e6bcae48ef9e2a6d21bf6d1845dbc2daa8520248
seodonghyunzz <durcjsay@gmail.com>
2024-10-21 23:56:38 +0900

    텍스트 수정, 수정취소기능 추가

--- app/component/todotable.js
+++ app/component/todotable.js
@@ -1,10 +1,10 @@
 'use client'
-import React from "react";
+import React, { useState } from 'react';
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+
 const TodoTable = ({fetchedtodos}) => {
     const router = useRouter()
-    
     const handleDelete = async (id) => {
         await fetch('http://localhost:3000/api/', {
           method: 'DELETE',
@@ -45,6 +45,52 @@ const TodoTable = ({fetchedtodos}) => {
         });
         setTimeout(() => { router.refresh() }, 0.1);
       }
+
+      const handleIsEditing = async (id) => {
+         await fetchedtodos.map((todo) => {
+          if(todo.id === id){
+            fetch('http://localhost:3000/api/', {
+              method: 'PUT',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({ 
+                id: todo.id,
+                todo: { ...todo, text: todo.text , isEditing: !todo.isEditing }
+            }),
+            });
+            setTimeout(() => { router.refresh() }, 0.1);
+          }
+        })
+      }
+      
+      const handleEdit = async ({ todo }) => {
+        await fetch('http://localhost:3000/api/', {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ 
+            id: todo.id,
+            todo: { ...todo, isEditing: false }
+        }),
+        });
+        setTimeout(() => { router.refresh() }, 0.1);
+      }
+      const handleEditCancel = async ({ todo }) => {
+        await fetch('http://localhost:3000/api/', {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ 
+            id: todo.id,
+            todo: { ...todo, text: todo.text , isEditing: false }
+        }),
+        });
+        setTimeout(() => { router.refresh() }, 0.1);
+      }
+      
     return (
         <div className="TodoTable">
             <table>
@@ -61,10 +107,21 @@ const TodoTable = ({fetchedtodos}) => {
                     <tr className={todo.isChecked ? "isChecked_tr" : ""}>
                         <td>{index+1}.</td>
                         <td>{todo.date}</td>
-                        <td>{todo.text}</td>
+                        {todo.isEditing ?
+                        (
+                          <td>
+                            <input type="text" defaultValue={todo.text} onChange={(e) => todo.text = e.target.value} onKeyDown={(e) => e.key === "Enter" && handleEdit({todo})}/>
+                            <button onClick={() => handleEdit({todo})}>Save</button>
+                            <button onClick={() => handleEditCancel({todo})}>x</button>
+                          </td>
+                        )
+                        :(
+                          <td>{todo.text}</td>
+                          
+                        )}
                         <td>
                             <button className="checkBtn" onClick={() => handleChecked({todo})}><Image src="/check.png" width={20} height={20} alt="check"/></button>
-                            <button className="editBtn" onClick={() => handleDelete(todo.id)}><Image src="/edit.png" width={20} height={20} alt="edit"/></button>
+                            <button className="editBtn" onClick={() => handleIsEditing(todo.id)}><Image src="/edit.png" width={20} height={20} alt="edit"/></button>
                             <button className="deleteBtn" onClick={() => handleDelete(todo.id)}><Image src="/delete.png" width={20} height={20} alt="delete"/></button>
                             <button className="priorityBtn" onClick={() => handlePriority({todo})}>{todo.isPriority ? <Image src="/fullstar.png" width={20} height={20} alt="fullstar"/>:<Image src="/emptystar.png" width={20} height={20} alt="emptystar"/>}</button>
                         </td>
